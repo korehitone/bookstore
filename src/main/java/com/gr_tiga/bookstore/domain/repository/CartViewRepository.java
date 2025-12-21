6b2753a46587173228796614680ad3e6c7d3c7d7
daryfmn <daryfmn@gmail.com>
2025-12-21 12:28:45 +0700

    latest version

--- src/main/java/com/gr_tiga/bookstore/domain/repository/CartViewRepository.java
+++ src/main/java/com/gr_tiga/bookstore/domain/repository/CartViewRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.gr_tiga.bookstore.model.view.CartView;
 
-public interface CartViewRepository extends JpaRepository<CartView, Integer> {
+public interface CartViewRepository extends JpaRepository<CartView, String> {
     
     CartView findByCustomerId(Integer custId);
 
